3c85cf0a94d74ef0b131780770d8c06298439363
Darlley <systembrtt@gmail.com>
2024-05-19 14:31:19 -0400

    fix: limpeza de componentes e navbar na página de login

--- src/app/(main)/agency/(auth)/layout.tsx
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Navigation from '@/components/site/navigation';
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
+    <>
+      <Navigation />
       <div className='w-full h-full flex items-center justify-center'>{children}</div>
+    </>
   );
 }
--- src/components/site/navigation.tsx
+++ src/components/site/navigation.tsx
@@ -11,15 +11,15 @@ type PropsType = {
 export default function navigation({ user }: PropsType) {
   return (
     <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
-      <aside className="flex items-center gap-2">
+      <Link href={'/site'} className="flex items-center gap-2">
         <Image
-          src={'./assets/plura-logo.svg'}
+          src={'/assets/plura-logo.svg'}
           width={40}
           height={40}
           alt="plur logo"
         />
         <span className="text-xl font-bold"> Plura.</span>
-      </aside>
+      </Link>
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
           <Link href={'#'}>Pricing</Link>
