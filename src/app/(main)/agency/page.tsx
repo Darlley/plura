406fb99646b4872f5303ba931fe34ba5477b872f
Darlley <systembrtt@gmail.com>
2024-05-22 22:28:43 -0400

    feat: criação de queries e seção da agencia

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,8 +1,15 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
 export default async function AgencyDashboardPage() {
-  const authUser = await currentUser();
-  if (!authUser) return redirect('/sign-in');
+  // const authUser = await currentUser();
+  // if (!authUser) return redirect('/sign-in');
+
+  const agencyId = await verifyAndAcceptInvitation()
+  console.log(agencyId)
+
+  const user = await getAuthUserDetails();
+
   return <div>Agency Dashboard Page</div>;
 }
--- src/components/site/navigation.tsx
+++ src/components/site/navigation.tsx
@@ -30,7 +30,7 @@ export default function navigation({ user }: PropsType) {
       </nav>
       <aside className="flex gap-2 items-center">
         <Link
-          href={'/sign-in'}
+          href={'/agency'}
           className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
         >
           Login
